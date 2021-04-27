7d5f53435d91d93c2f6032f50ffc2db8bca6416e
musa <mosesbwire91@gmail.com>
2021-04-27 20:22:17 +0300

    generate invitations controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -8,7 +8,6 @@ class EventsController < ApplicationController
     end
 
     def create
-    
         @event = current_user.events.build(event_params)
         if @event.save
             flash[:success] = "Event successfully created"
