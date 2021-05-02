bc5e4de7db932756b1673f8cb8cf1a4b0fbf003c
musa <mosesbwire91@gmail.com>
2021-05-02 19:45:48 +0300

    add more styling to application

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -2,8 +2,10 @@ class InvitationsController < ApplicationController
     def create
         invited_guests = params[:guest]
         event_id = params[:event_id]
+        
         invited_guests.each do |guest|
-            Invitation.create(user_id: guest, event_id: event_id)
+            msg = "Dear #{User.find(guest).username}, u have been invited to the party"
+            Invitation.create(user_id: guest, event_id: event_id, message: msg)
         end
         redirect_to events_path
     end
