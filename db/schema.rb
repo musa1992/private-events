ae2024f92fe36bfe12f7525892f6b814bac3ba79
musa <mosesbwire91@gmail.com>
2021-05-02 16:21:21 +0300

    add message column to invitation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_29_200008) do
+ActiveRecord::Schema.define(version: 2021_05_02_132018) do
 
   create_table "events", force: :cascade do |t|
     t.datetime "event_date"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2021_04_29_200008) do
     t.integer "event_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "message"
     t.index ["event_id"], name: "index_invitations_on_event_id"
     t.index ["user_id"], name: "index_invitations_on_user_id"
   end
